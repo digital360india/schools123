2aabf85125ce3d9008d96d182c78f526e0eb3288
Praveen <praveennegi700@gmail.com>
2024-12-27 14:57:00 +0530

    banner change schools123

--- components/HeroSchool123.jsx
+++ components/HeroSchool123.jsx
@@ -5,26 +5,19 @@ import Image from "next/image";
 
 const HeroSchool123 = () => {
   return (
-    <div className="relative mt-20 bg-green-700 w-full h-[100vh]">
+    <div className="relative mt-24 w-full h-full">
       <Image
-        src="/heroschool123.svg"
+        src="/heroschool123.png"
         alt="School choice"
         width={1000}
         height={1000}
-        className="w-full h-[100vh] object-cover hidden md:block"
+        className="w-full h-[70vh] object-cover hidden md:block"
       />
 
-      <div className="absolute top-[25%] md:top-1/2 right-10 transform -translate-y-1/2 text-white px-4 md:px-8 lg:right-20 lg:top-1/2 lg:transform lg:-translate-y-1/2">
-        <p className="text-[20px] sm:text-[30px] md:text-[35px] lg:text-[40px] font-light Merriweather text-center justify-center lg:text-right">
-          Find the <span className="font-bold">Perfect School</span> for Every Child
-        </p>
-        <p className="text-[30px] sm:text-[40px] md:text-[50px] lg:text-[60px] font-bold mt-2 text-center lg:text-right Merriweather">
-          Schools 123
-        </p>
-      </div>
+     
       <div className="md:hidden">
       <Image
-        src="/herofram.svg"
+        src="/heroschool123.png"
         alt="School choice"
         width={1000}
         height={1000}
